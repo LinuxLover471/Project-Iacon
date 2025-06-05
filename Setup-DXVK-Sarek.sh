7cab8521082238b50e231d8f75edf3ad864b3cdb
LinuxLover471 <Garima60000@gmail.com>
2025-06-05 18:11:40 +0530

    Setting up repo

--- Setup-DXVK-Sarek.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-dir="$(dirname "$(readlink -f "$0")")"
-cd ${dir}/dxvk-sarek-async-v1.10.9
-WINEPREFIX=~/.wine
-cp -v x64/*.dll $WINEPREFIX/drive_c/windows/system32
-cp -v x32/*.dll $WINEPREFIX/drive_c/windows/syswow64
-echo "Please add d3d9 d3d10core d3d11 dxgi"
-winecfg
