1f60d25ea1a18a0626e803f96f7e2b79a72dd9ec
LinuxLover471 <garima60000@gmail.com>
2026-01-13 17:44:03 +0530

    Add ananicy-cpp rules and improve some formatting.

--- scripts/1-misc-commands.sh
+++ scripts/1-misc-commands.sh
@@ -15,17 +15,20 @@ sudo systemctl enable nftables
 if [[ "$ananicyornot" == "ananicy-cpp" ]]; then
   echo "Enabling ananicy-cpp and setting it's configuration"
   sudo systemctl enable ananicy-cpp.service
-  sudo cp -v "$dir"/Ananicy/ananicy.d/* /etc/ananicy.d/
+  sudo cp -v "$dir"/ananicy-rules/* /etc/ananicy.d/
+
 elif [[ "$ananicyornot" == "gamemode" ]]; then
   echo "Adding $user to gamemode user group."
   sudo usermod -aG gamemode "$user"
   echo "Setting up gamemode.ini for better niceness."
   sudo cp -v "$dir"/config/gamemode.ini /etc/gamemode.ini
+
 else
   # Ananicy-cpp.
   echo "Enabling ananicy-cpp and copying rules."
   sudo systemctl enable ananicy-cpp.service
-  sudo cp -v "$dir"/Ananicy/ananicy.d/* /etc/ananicy.d/
+  sudo cp -v "$dir"/ananicy-rules/* /etc/ananicy.d/
+
   # Gamemode.
   echo "Adding $user to gamemode user group."
   sudo usermod -aG gamemode "$user"
--- scripts/6-install-de.sh
+++ scripts/6-install-de.sh
@@ -7,27 +7,33 @@ if [[ "$de_type" == "KDE" ]]; then
     echo "Installing minimal version of KDE, with a few packages."
     sudo pacman -S --needed --noconfirm $de_pkg
     sudo systemctl enable sddm
+
   elif [[ "$kdetype" == "Meta" ]]; then
     echo "Installing Meta version of KDE."
     sudo pacman -S --needed --noconfirm $de_pkg
     sudo systemctl enable sddm
+
   elif [[ "$kdetype" == "Full" ]]; then
     echo "Installing full version of KDE."
     sudo pacman -S --needed --noconfirm $de_pkg
     sudo systemctl enable sddm
   fi
+
 elif [[ "$de_type" == "XFCE" ]]; then
   echo "Installing XFCE."
   sudo pacman -S --noconfirm --needed $de_pkg
   sudo systemctl enable lightdm
+
 elif [[ "$de_type" == "Gnome" ]]; then
   echo "Installing Gnome."
   sudo pacman -S --needed --noconfirm $de_pkg
   sudo systemctl enable gdm
+
 elif [[ "$de_type" == "i3" ]]; then
   echo "Installing i3."
   sudo pacman -S --needed --noconfirm $de_pkg
   sudo systemctl enable ly
+
 else
   echo "Invalid DE choice: $de_type"
   exit 1
